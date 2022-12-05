e06996d09191a3703d689957181002dc056dc302
Alexandr Goncharuk <Alexandr.Goncharuk@akurateco.com>
2022-12-05 12:28:53 +0200

    xe.v1 qq

--- src/Delivery/Infrastructure/Services/CalculateDelivery.php
+++ src/Delivery/Infrastructure/Services/CalculateDelivery.php
@@ -7,34 +7,27 @@ use App\Delivery\Domain\Interfaces\CalculateDeliveryInterface;
 
 class CalculateDelivery implements CalculateDeliveryInterface
 {
-
     private $priceLoader;
 
     /**
-     * @param $priceLoader
+     * @param LoadDeliveryPrice $priceLoader
      */
     public function __construct($priceLoader)
     {
         $this->priceLoader = $priceLoader;
     }
 
-    /**
-     * @param Parcel $parcel
-     * @return float
-     */
     public function calc(Parcel $parcel): float
     {
         $price = $this->priceLoader->load_parcel_price($parcel);
+
         return $price['parcel_type'] + $this->calculate_options($parcel);
     }
 
-    /**
-     * @param Parcel $parcel
-     * @return float|int
-     */
-    public function calculate_options(Parcel $parcel)
+    public function calculate_options(Parcel $parcel): float
     {
         $price = $this->priceLoader->load_parcel_price($parcel);
-        return array_sum($price['options']);
+
+        return (float)array_sum($price['options']);
     }
 }
--- src/Delivery/Infrastructure/Validator/IsOptionExist.php
+++ src/Delivery/Infrastructure/Validator/IsOptionExist.php
@@ -6,9 +6,6 @@ use Symfony\Component\Validator\Constraint;
 
 class IsOptionExist extends Constraint
 {
-    /**
-     * @var string
-     */
-    public string $option;
+    public string $option = '';
     public string $message = 'Invalid options value';
 }
